70d061da53a4a4eeefa05acbece34f5419b533eb
EverardoMeireles <everardomeireles@gmail.com>
2022-12-08 15:02:50 -0300

    -Progress on animation, smoothstep algorithm

--- src/SceneContainer.jsx
+++ src/SceneContainer.jsx
@@ -4,15 +4,43 @@ import { Test_scene } from "./Test_scene";
 // import { Tube } from "./Tube";
 import {useSpring, a} from '@react-spring/three'
 import * as THREE from "three";
-import { useThree } from '@react-three/fiber'
+import { useFrame, useThree, useStore } from '@react-three/fiber'
+import { Vector3 } from "three";
+import { useTimer } from 'use-timer';
+import { smoothstep } from "three/src/math/MathUtils";
+
 
 
 export function SceneContainer() {
 
+    // const path = new THREE.CatmullRomCurve3( [
+    //     new THREE.Vector3( 0, 0, 0 ),
+    //     new THREE.Vector3( 500, 200, 400 ),
+    //     new THREE.Vector3( 150, 50, 100 ),
+    //     new THREE.Vector3( 250, 25, 45 ),
+    //     new THREE.Vector3( 58, 2, 36 )
+    // ] );
+
     const path = new THREE.CatmullRomCurve3( [
-        new THREE.Vector3( 0, 0, 0 ),
-        new THREE.Vector3( 5, -5, 5 ),
-        new THREE.Vector3( 10, 0, 10 )
+        // new THREE.Vector3( 50, 0, 0 ),
+        // new THREE.Vector3( 0, 0, 0 ),
+        // new THREE.Vector3( 0, 0, 50 ),
+
+        new THREE.Vector3( 15, 0, 0 ),
+        new THREE.Vector3( 15, 10, -15 ),
+        new THREE.Vector3( 15, 10, -30 ),
+
+
+
+        // new THREE.Vector3( 10, 24, 14 ),
+        // new THREE.Vector3( 30, 36, 16 ),
+        // new THREE.Vector3( 40, 60, 30 ),
+
+        // new THREE.Vector3( 10, 24, 14 ),
+        // new THREE.Vector3( 25, 42, 22 ),
+        // new THREE.Vector3( 40, 60, 30 ),
+
+        
     ] );
 
     const [active, setActive] = useState(false)
@@ -25,14 +53,25 @@ export function SceneContainer() {
     
 })
     const Tube = () => {
+        const aaa = useRef()
+        useEffect(() => {
+            console.log(aaa);
+            aaa.current.wireframe = true;
+            aaa.current.visible = true;
+            aaa.current.position = new THREE.Vector3(12, 10.85, 30);
+          }, [])
+
+        // aaa.current.wireframe = "true;
+        
         return(
-            <a.mesh
+            <mesh
                 onClick={() => setActive(!active)}
                 // position={props.position}
             >
-                <tubeGeometry args={[path, 70, 2, 50, false]} />
-                <meshStandardMaterial attach="material" color={0x00ff00}/>
-            </a.mesh>
+                <tubeGeometry args={[path, 40, 1, 70, false]} />
+                <meshStandardMaterial wireframe="true" ref={aaa}  attach="material"/>
+                
+            </mesh>
         )
     }
 
@@ -48,38 +87,64 @@ export function SceneContainer() {
         )
     }
 
+    function smoothStep(x) { //Normal smoothstep
+        return -2 * Math.pow(x, 3) + 3 * Math.pow(x, 2);
+      }
+
     const Camera1 = () => {
-        // const camera = useThree((state) => state.camera)
-        var camPos = new THREE.Vector3(12, 10.85, 30);       // Holds current camera position
-        var targetPos = new THREE.Vector3(10, 10, -10);// Target position
-        var origin = new THREE.Vector3(98, 99, 87);       // Optional origin
-        const cam = useRef()
+        var current_path = "index";
+        const cam = useRef();
+        const { time, start, pause, reset, status } = useTimer();
+        const [clicked, setCount] = useState("dsq");
+
         useEffect(() => {
             console.log(cam);
+            cam.current.zoom = 1
+            
           }, [])
-        function animate(){
-            // // Interpolate camPos toward targetPos
-            // camPos.lerp(targetPos, 0.05);
+          
+        // useFrame( () => (camPos.x += 0.3));
 
-            // // // Apply new camPos to your camera
-            // state.camera.position.copy(camPos);
+        // useFrame( () => (cam.current.position.x += 0.1, cam.current.position.y += 0.1));
+        var posInit = path.getPointAt(0);
+        var tick = 0;
+        var sub_points;
+        var vector = new THREE.Vector3( 0, 0, -1 );
+        var vector_look_at = new THREE.Vector3( -24, 65, -43 );
+        var smooth;
+        // make sure to use a timer to tick the animation
+        // implement buttonclick
+        // add support for multiple paths
+        useFrame((state) => (tick <= 1 ?(
+            console.log("IS RUNNING"),
+            vector.lerp(vector_look_at, 0.01),
+            state.camera.lookAt(vector),
+            tick += 0.005,
+            //  tick+=0.005 : tick = 1,
+            smooth = smoothStep(tick) <= 1 ? smoothStep(tick) : 1,
+            sub_points = path.getPointAt(smooth),
+            // (tick) => (sub_points != tick) ? console.log("d") : console.log("s"),
+            state.camera.position.x = sub_points.x,
+            state.camera.position.y = sub_points.y,
+            state.camera.position.z = sub_points.z)
+            : state.camera.lookAt(vector)
+        ));
+
+        // useFrame(() => {
+        //     if (time >= 2) {
+        //      pause();
+        //      reset();
+        //     }
+        //    })
+
+        // useFrame(()=>(console.log(path.getPointAt(tt))))
+        // useFrame( () => (
+        //     console.log(smooth)
+        //     // console.log(tick)
+        //     ));
         
-            // (Optional) have camera look at the origin after it's been moved
-            // state.camera.lookAt(origin);
-        
-            // render();
-            camPos.x = 888;
-            // cam.current.position = 12
-            
-
-
-        }
-        animate();
         return(
-            <PerspectiveCamera ref={cam} makeDefault fov={50} position={camPos} />
-
-            
-
+            <PerspectiveCamera ref={cam} makeDefault fov={75} position={posInit} />
         )
     }
 
