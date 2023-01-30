4cece5af71116d4b60434e303ff1fc48af387530
EverardoMeireles <everardomeireles@gmail.com>
2023-01-30 05:27:59 -1200

    Improved BaseCube.jsx and IndexMenu.jsx by giving the base cube more parameters and options

--- src/components/BaseCube.jsx
+++ src/components/BaseCube.jsx
@@ -3,6 +3,7 @@ import {useSpring, a} from '@react-spring/three';
 
 export function BaseCube(props) {
     // default prop values:
+    const {position = [0, 0, 0]} = props;
     const {width = 1} = props;
     const {height = 1} = props;
     const {depth = 1} = props;
@@ -11,17 +12,20 @@ export function BaseCube(props) {
     const {minMovementDelay = 150} = props;
     const {maxMovementDelay = 500} = props;
     const {animationDelay = Math.floor(Math.random() * (maxMovementDelay - minMovementDelay) + maxMovementDelay)} = props;
+    const {hasMovementAnimation = true} = props;
+    const {hasScaleAnimation = true} = props;
+
 
     const minColorValue = 200;//180 180 180 light grey
     const maxColorValue = 255;//255 255 255 white
-    const randcolor = Math.floor(Math.random() * (maxColorValue - minColorValue) + minColorValue);
+    const randcolor = hasMovementAnimation ? Math.floor(Math.random() * (maxColorValue - minColorValue) + minColorValue) : 255;
 
     const [hovered, setHover] = useState(false);
 
     const springPosition = useSpring({
         loop: {reverse: true},
-        from: {position: props.position},
-        to: {position: [props.position[0] + movementVector[0], props.position[1] + movementVector[1], props.position[2] + movementVector[2]]},
+        from: {position: position},
+        to: {position: [position[0] + movementVector[0], position[1] + movementVector[1], position[2] + movementVector[2]]},
         cancel: false,
         config: {
             duration: animationDelay
@@ -40,8 +44,8 @@ export function BaseCube(props) {
     <a.mesh
     onPointerOver={() => setHover(true)}
     onPointerOut={() => setHover(false)}
-    position = {springPosition.position}
-    scale={springScale.scale}
+    position = {hasMovementAnimation ? springPosition.position : position}
+    scale = {hasScaleAnimation ? springScale.scale : 1}
     >
         {props.children}
         <boxGeometry  args={[width, height, depth]}/>
--- src/components/IndexMenu.jsx
+++ src/components/IndexMenu.jsx
@@ -5,13 +5,23 @@ import * as THREE from "three";
 
 export function IndexMenu(props) {
     const {setPath, setTransitionEnded} = props.useStore();
+    const {rotation = 2 * Math.PI} = props;
+    const {scale = 1} = props;
+    const {textColor = "#000000"} = props;
 
-    const callbackRef = useCallback(
+    
+    const textCallbackRef = useCallback(
         ref => ref != null ? (ref.setRotationFromAxisAngle(new THREE.Vector3(0, 1, 0) , (Math.PI/2))):console.log("skip render")
         )
 
+    const wholeCallbackRef = useCallback(
+        ref => ref != null ? (ref.setRotationFromAxisAngle(new THREE.Vector3(0, 1, 0) , (rotation))):console.log("skip render")
+        )
+
     return(
-        <>
+        <mesh 
+        scale={scale}
+        ref={wholeCallbackRef}>
             <mesh
             onClick = {(e) => {
                 e.stopPropagation();
@@ -24,25 +34,92 @@ export function IndexMenu(props) {
                 position={[0,0,0]} 
                 width={1} 
                 height={1} 
-                depth={5}
+                depth={10}
+                hasMovementAnimation={false}
+                hasScaleAnimation={false}
+                hasChangeColorOnHover={false}
                 >
                     <Suspense fallback = {null}>
                         <Text3D
-                        position = {[0.5, -0.25, 1.5]}//Use a more standardised approach
-                        ref = {callbackRef}
+                        position = {[0.5, -0.25, 1.7]}//Use a more standardised approach
+                        ref = {textCallbackRef}
                         font = {process.env.PUBLIC_URL + "/roboto.json"}
                         size = {0.575}
                         height = {0.065}
                         curveSegments = {12}
                         >
-                            Projects
-                            <meshStandardMaterial color = {[1, 0.15, 0.1]} emissive = {[1, 0.1, 0]} />
+                            Education
+                            <meshStandardMaterial color = {textColor} />
+                        </Text3D>
+                    </Suspense>
+                </BaseCube>
+            </mesh>
+            <mesh
+            onClick = {(e) => {
+                e.stopPropagation();
+                setPath("Competences");
+                setTransitionEnded(false);
+            }}
+            position = {[0,1,2]}
+            >
+                <BaseCube
+                position={[0,0,0]} 
+                width={1}
+                height={1}
+                depth={10}
+                hasMovementAnimation={false}
+                hasScaleAnimation={false}
+                hasChangeColorOnHover={false}
+                >
+                    <Suspense fallback = {null}>
+                        <Text3D
+                        position = {[0.5, -0.25, 2.5]}//Use a more standardised approach
+                        ref = {textCallbackRef}
+                        font = {process.env.PUBLIC_URL + "/roboto.json"}
+                        size = {0.575}
+                        height = {0.065}
+                        curveSegments = {12}
+                        >
+                            Compétences
+                            <meshStandardMaterial color = {textColor} />
+                        </Text3D>
+                    </Suspense>
+                </BaseCube>
+            </mesh>
+            <mesh
+            onClick = {(e) => {
+                e.stopPropagation();
+                setPath("experiencePro");
+                setTransitionEnded(false);
+            }}
+            position = {[0,2,2]}
+            >
+                <BaseCube
+                position={[0,0,0]} 
+                width={1} 
+                height={1} 
+                depth={10}
+                hasMovementAnimation={false}
+                hasScaleAnimation={false}
+                hasChangeColorOnHover={false}
+                >
+                    <Suspense fallback = {null}>
+                        <Text3D
+                        position = {[0.5, -0.25, 4.7]}//Use a more standardised approach
+                        ref = {textCallbackRef}
+                        font = {process.env.PUBLIC_URL + "/roboto.json"}
+                        size = {0.575}
+                        height = {0.065}
+                        curveSegments = {12}
+                        >
+                            Éxperience professionnelle
+                            <meshStandardMaterial color = {textColor}  />
                         </Text3D>
                     </Suspense>
                 </BaseCube>
             </mesh>
 
-        <BaseCube position = {[0,1,0]} movementVector = {[0.1, 0, 0]} />
+        {/* <BaseCube position = {[0,1,0]} movementVector = {[0.1, 0, 0]} />
         <BaseCube position = {[0,2,0]} />
         <BaseCube position = {[0,3,0]} />
         <BaseCube position = {[0,4,0]} />
@@ -71,7 +148,7 @@ export function IndexMenu(props) {
         <BaseCube position = {[0,3,4]} />
         <BaseCube position = {[0,4,4]} />
         <BaseCube position = {[0,5,4]} />
-        <BaseCube position = {[0,6,4]} />
-    </>
+        <BaseCube position = {[0,6,4]} /> */}
+    </mesh>
     );
 }
