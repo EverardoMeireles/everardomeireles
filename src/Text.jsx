ab13f1444c44c08f7736e629fc0ce7a6b3e07310
EverardoMeireles <everardomeireles@gmail.com>
2023-02-02 03:38:24 -1200

    Added a Text.jsx file so in the future, all text can be written here to change between languages

---
+++ src/Text.jsx
@@ -0,0 +1,45 @@
+export const Text = {
+
+        "PROSPERE-ITB-PRESENTATION":
+        {
+            "FR":"Prospere ITB est une entreprise d'e-learning    brésilienne. J'ai aidé Fernando,  le développeur de l'entreprise a résoudre des problèmes dans  le backlog de l'application web dédiée aux cours en ligne. J'ai utilisé le language C# et le    framework ASP.NET.",
+            "EN":"",
+
+        },
+
+        "DRIM-PRESENTATION":
+        {
+            "FR":"Drim est une entreprise spécialisée dans les    composants d'ascenseurs et leurs systèmes       embarqués. Mon travail consistait développer un système de commande de plafonnier en 3D avec    THREE.js et à faire des ajustements dans le site de l'entreprise avec le cms wordpress.",
+            "EN":"",
+        },
+
+        "EVERIAL-PRESENTATION":
+        {
+            "FR":"Service études et développement, j'ai intégré   l'équipe de développement de l'application WPF  de l'entreprise qui est utilisé en interne.     J'ai utilisé le langage de programmation C#,    avec mon équipe on utilisait tfs pour le        contrôle de version et la méthode Agile de      développement.",
+            "EN":"",
+        },
+
+        "BRESIL-ECOBUGGY-PRESENTATION":
+        {
+            "FR":"Brésil eco-buggy est une entreprise de tourisme au Brésil qui fait des expéditions en buggy de longues distances au long de la côte du nord-est Brésilien. En tant qu'organisateur d'expédition mes principales responsabilités étaient d'être contact avec les clients et de donner des devis d'hôtels et de transport. Je m'occupais aussi de problèmes avec le réseau de l'entreprise et les ordinateurs.",
+            "EN":"",
+        },
+
+        "EFN1-PRESENTATION":
+        {
+            "FR":"En conformité avec les exigences de l'Éducation       nationale pour la mise en place du PPMS dans les      écoles françaises, j'ai créé un système d'alarme pour l'école française de Natal, au Brésil. L'école ma     demandé de développer un système d'alarme sans fil    dans les salles de classe, que j'ai réalisé avec le   C++(framework arduino) et l'électronique.",
+            "EN":"",
+        },
+
+        "EFN2-PRESENTATION":
+        {
+            "FR":"Pendant trois mois, j'ai donné des cours  de sensibilisation à la programmation     (Scratch) et tableurs Excel à des classes de CM1 et CM2 à l'école française de Natal",
+            "EN":"",
+        },
+        
+        // "EVERIALPRESENTATION":"",
+        // "BRESILECOBUGGYPRESENTATION":"",
+        // "EFNPRESENTATION":"",
+        // "EFNPRESENTATION2":"",
+        // "PROEXPPROJPRESENTATION6":"",
+    }
