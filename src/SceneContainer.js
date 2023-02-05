8d3e40da7e90909f22f007590a55bce8e3d10907
EverardoMeireles <everardomeireles@gmail.com>
2023-02-05 09:15:27 -1200

    Updated SceneContainer

--- src/SceneContainer.js
+++ src/SceneContainer.js
@@ -1,5 +1,5 @@
 import { Environment } from "@react-three/drei";
-import { Suspense } from "react";
+import { Suspense, useEffect, useRef } from "react";
 import { Camera } from "./components/Camera";
 import { SimpleLoader } from "./components/SimpleLoader";
 import create from 'zustand';
@@ -33,27 +33,41 @@ export function SceneContainer() {
     const currentGraphicalMode = useStore((state) => state.currentGraphicalMode);
     const finishedBenchmark = useStore((state) => state.finishedBenchmark);
 
+const directSun = useRef()
+
+useEffect(()=>{
+    console.log(directSun.current)
+    // directSun.current.target.position = [-38, -1, 66]
+    // directSun.current.target.position = [-38, -1, 66]
+})
+
     return(
         <>
+                        {/* <directionalLight ref={directSun} intensity={2} target-position={[-38, -1, 66]}  position={[-19, 18, 97]}></directionalLight>  */}
+            {/* <pointLight position={[-32, 16, 91]} intensity={.3} ></pointLight> */}
             <Suspense fallback = {null} >
                 {/* <SimpleLoader></SimpleLoader> */}
-                {/* <directionalLight intensity={0.4} position={[63,96,41]}></directionalLight> */}
+                {/* <directionalLight  intensity={.4} position={[63,96,41]}></directionalLight>  */}
                 <Environment files={process.env.PUBLIC_URL + "/textures/dikhololo_night_1k.hdr"} background />
                 <Environment files={process.env.PUBLIC_URL + "/textures/kloofendal_48d_partly_cloudy_puresky_1k.hdr"} background={"only"} />
-                {/* <ambientLight intensity={0.1}></ambientLight> */}
+                {/* <OrbitingPointLight lightIntensivity={.3}></OrbitingPointLight> */}
                 {finishedBenchmark == false && <GraphicalModeSetter {...{useStore}} />}
                 <Camera {...{useStore}}></Camera>
                 <FloatingTextSkills initialPosition={[-41, 0, 68]} rotation={3*(Math.PI/2)}></FloatingTextSkills >
-                <OrbitingMenu orbitDistance={7.5} orbitCenterPosition={[-17, -34, -4]}/>
+                
+                {/* <OrbitingPointLight orbitDirection={[0, 0, 1]} orbitAxis={"y"} orbitDistance={8} orbitCenterPosition={[-34, -1, 70]} lightIntensivity={2}></OrbitingPointLight> */}
+
+
+                <OrbitingMenu orbitDistance={7.5} orbitCenterPosition={[-6, -35, -4]}/>
                 {/* <FadingSlideShowModel {...{useStore}} />
                 <FadingTextModel {...{useStore}} initialPosition={[0, 0, -10]} ></FadingTextModel> */}
-                <SimpleLoader></SimpleLoader>
-                <FadingTextModel {...{useStore}} textToFade={Text["PROSPERE-ITB-PRESENTATION"]["FR"]} textModelMenu="ProfessionalExpProjects0" scale={2} initialPosition={[1, 4, -82]} rotation={2 * Math.PI} visible={false} textColor={"#FFFFFF"} text3dMode={false}/>
-                <FadingTextModel {...{useStore}} textToFade={Text["DRIM-PRESENTATION"]["FR"]} textModelMenu="ProfessionalExpProjects1" scale={1.8} initialPosition={[11.5, 5.5, -94]} rotation={Math.PI/2} visible={false} textColor={"#FFFFFF"} text3dMode={false}/>
-                <FadingTextModel {...{useStore}} textToFade={Text["EVERIAL-PRESENTATION"]["FR"]} textModelMenu="ProfessionalExpProjects2" scale={2} initialPosition={[-2, 5.5, -105]} rotation={Math.PI} visible={false} textColor={"#FFFFFF"} text3dMode={false}/>
-                <FadingTextModel {...{useStore}} textToFade={Text["BRESIL-ECOBUGGY-PRESENTATION"]["FR"]} textModelMenu="ProfessionalExpProjects3" lettersPerUnit={10} scale={1.7} initialPosition={[-11, 7.5, -94.5]} rotation={3*(Math.PI/2)} visible={false} textColor={"#FFFFFF"} text3dMode={false}/>
-                <FadingTextModel {...{useStore}} textToFade={Text["EFN1-PRESENTATION"]["FR"]} textModelMenu="ProfessionalExpProjects4" lettersPerUnit={9} scale={1.8} initialPosition={[-0.5, 27, -82.2]} rotation={2 * Math.PI} visible={false} textColor={"#FFFFFF"} text3dMode={false}/>
-                <FadingTextModel {...{useStore}} textToFade={Text["EFN2-PRESENTATION"]["FR"]} textModelMenu="ProfessionalExpProjects5" scale={2.5} lettersPerUnit={7} initialPosition={[11.5, 23, -96]} rotation={Math.PI/2} visible={false} textColor={"#FFFFFF"} text3dMode={false}/>
+                {/* <SimpleLoader></SimpleLoader> */}
+                <FadingTextModel {...{useStore}} textToFade={Text["PROSPERE-ITB-PRESENTATION"]["FR"]} textModelMenu="ProfessionalExpProjects0" scale={2} initialPosition={[1, 4, -82]} rotation={2 * Math.PI} visible={false} textColor={"#FFFFFF"} istext3d={false}/>
+                <FadingTextModel {...{useStore}} textToFade={Text["DRIM-PRESENTATION"]["FR"]} textModelMenu="ProfessionalExpProjects1" scale={1.8} initialPosition={[11.5, 5.5, -94]} rotation={Math.PI/2} visible={false} textColor={"#FFFFFF"} istext3d={false}/>
+                <FadingTextModel {...{useStore}} textToFade={Text["EVERIAL-PRESENTATION"]["FR"]} textModelMenu="ProfessionalExpProjects2" scale={2} initialPosition={[-2, 5.5, -105]} rotation={Math.PI} visible={false} textColor={"#FFFFFF"} istext3d={false}/>
+                <FadingTextModel {...{useStore}} textToFade={Text["BRESIL-ECOBUGGY-PRESENTATION"]["FR"]} textModelMenu="ProfessionalExpProjects3" lettersPerUnit={10} scale={1.7} initialPosition={[-11, 7.5, -94.5]} rotation={3*(Math.PI/2)} visible={false} textColor={"#FFFFFF"} istext3d={false}/>
+                <FadingTextModel {...{useStore}} textToFade={Text["EFN1-PRESENTATION"]["FR"]} textModelMenu="ProfessionalExpProjects4" lettersPerUnit={9} scale={1.8} initialPosition={[-0.5, 27, -82.2]} rotation={2 * Math.PI} visible={false} textColor={"#FFFFFF"} istext3d={false}/>
+                <FadingTextModel {...{useStore}} textToFade={Text["EFN2-PRESENTATION"]["FR"]} textModelMenu="ProfessionalExpProjects5" scale={2.5} lettersPerUnit={7} initialPosition={[11.5, 23, -96]} rotation={Math.PI/2} visible={false} textColor={"#FFFFFF"} istext3d={false}/>
                 {/* <FadingTextModel {...{useStore}} textModelMenu="ProfessionalExpProjects6" initialPosition={[-4, 28, -105]} rotation={Math.PI} visible={false} textColor={"#FFFFFF"} text3dMode={false}/>
                 <FadingTextModel {...{useStore}} textModelMenu="ProfessionalExpProjects7" initialPosition={[-11, 28, -90]} rotation={3*(Math.PI/2)} visible={false} textColor={"#FFFFFF"} text3dMode={false}/>
                 <FadingTextModel {...{useStore}} textModelMenu="ProfessionalExpProjects8" initialPosition={[4, 49, -82.2]} rotation={2 * Math.PI} visible={false} textColor={"#FFFFFF"} text3dMode={false}/>
@@ -63,33 +77,27 @@ export function SceneContainer() {
                 <ExperienceMenu {...{useStore}} scale={1} /*position={[-7, 0, -81]} rotation={-Math.PI/3}*/></ExperienceMenu>
                 <Title initialPosition={[0, 4, 5.4]}/>
                 <IndexMenu {...{useStore}} isMainMenu></IndexMenu>
-                <IndexMenu {...{useStore}} position={[-24, -37.6, -0.5]} rotation={2 * Math.PI/12} scale={0.23} ></IndexMenu>
+                <IndexMenu {...{useStore}} position={[-13, -38.5, -1.3]} rotation={2 * Math.PI/12} scale={0.23} ></IndexMenu>
                 <IndexMenu {...{useStore}} position={[-50, -2, 65]} rotation={  Math.PI} scale={0.35} ></IndexMenu>
-                
+                {/* <ambientLight intensity={1}></ambientLight> */}
+
             </Suspense>
             {(currentGraphicalMode == "potato")
             &&
             <group>
                 <Suspense fallback = {null}>
-                    <ambientLight />
+                    <SimpleLoader modelName={"threeJsScenePotato.glb"}></SimpleLoader>
+                    <ambientLight intensity={1}></ambientLight>
                     {/* <IndexMenu {...{useStore}}></IndexMenu>
                     <ProjectsMenu {...{useStore}} /> */}
-                </Suspense>
-            </group>}
-            {(currentGraphicalMode == "potatoExtra")
-            && 
-            <group>
-                <Suspense fallback = {null}>
-                    {/* <OrbitingPointLight></OrbitingPointLight> */}
-                    {/* <IndexMenu {...{useStore}}></IndexMenu>
-                    <ProjectsMenu {...{useStore}} />  */}
-                    {/* <SimpleLoader></SimpleLoader> */}
                 </Suspense>
             </group>}
             {(currentGraphicalMode == "potatoPremium")
             && 
             <group>
                 <Suspense fallback = {null}>
+                    <SimpleLoader modelName={"threeJsSceneNormal.glb"}></SimpleLoader>
+                    
                     {/* <OrbitingPointLight></OrbitingPointLight> */}
                     {/* <IndexMenu {...{useStore}}></IndexMenu>
                     <ProjectsMenu {...{useStore}} /> */}
@@ -100,6 +108,7 @@ export function SceneContainer() {
             && 
             <group>
                 <Suspense fallback = {null}>
+                    <SimpleLoader modelName={"threeJsSceneNormal.glb"}></SimpleLoader>
                     {/* <OrbitingPointLight></OrbitingPointLight> */}
                     {/* <IndexMenu {...{useStore}}></IndexMenu>
                     <ProjectsMenu {...{useStore}} /> */}
@@ -108,6 +117,12 @@ export function SceneContainer() {
             {(currentGraphicalMode == "high")
             && <group>
                 <Suspense fallback = {null}>
+                <SimpleLoader modelName={"threeJsSceneNormal.glb"}></SimpleLoader>
+                <OrbitingPointLight orbitDirection={[0, 1, 0]} orbitSpeed={0.01} orbitAxis={"x"} orbitDistance={60} orbitCenterPosition={[-40, 30, 0]} lightIntensivity={10}></OrbitingPointLight>
+                {/* <pointLight position={[-11, 3, -124]}></pointLight> */}
+                {/* <SimpleLoader modelName={"threeJsScenePotato.glb"}></SimpleLoader> */}
+                {/* <directionalLight  intensity={.4} position={[63,96,41]}></directionalLight>  */}
+                {/* <OrbitingPointLight orbitDirection={[0, 1, 0]} orbitSpeed={0.05} orbitAxis={"x"} orbitDistance={2} orbitCenterPosition={[0, 0, 0]} lightIntensivity={2}></OrbitingPointLight> */}
                 {/* <OrbitingMenu orbitCenterPosition={[15.5, 1.1, 0]}></OrbitingMenu> */}
                 {/* <OrbitingPointLight></OrbitingPointLight> */}
                 {/* <IndexMenu {...{useStore}}></IndexMenu>
