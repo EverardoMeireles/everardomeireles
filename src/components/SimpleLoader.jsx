13a1b296fd1892cb5edd19a7127cd4d9b554e144
EverardoMeireles <everardomeireles@gmail.com>
2023-03-25 06:32:57 -1200

    Memoized SimpleLoader.jsx

--- src/components/SimpleLoader.jsx
+++ src/components/SimpleLoader.jsx
@@ -2,23 +2,13 @@ import { useLoader, useFrame } from '@react-three/fiber'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
 import { useCallback, Suspense } from "react";
 import * as THREE from "three";
-import { useEffect } from "react";
+import React, { useEffect, useRef } from "react";
 import { BufferAttribute, Color } from "three";
 
-
-
-export function SimpleLoader(props) {
+export const SimpleLoader = React.memo((props) => {
     const {modelName = "threeJsScene.glb"} = props;
 
     const gltf = useLoader(GLTFLoader, process.env.PUBLIC_URL + '/models/' + modelName)
-
-    useEffect(() => {
-        if(!gltf) return;
-
-        let mesh = gltf.scene.children[0];
-        console.log(gltf);
-
-    }, [gltf]);
 
     let mixer
     if (gltf.animations.length) {
@@ -36,7 +26,6 @@ export function SimpleLoader(props) {
     return (
     <Suspense fallback={null}>
         <primitive object={gltf.scene} />
-        {/* <meshStandardMaterial side={THREE.DoubleSide}></meshStandardMaterial> */}
     </Suspense>
     )
-  }
+})
