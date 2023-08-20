2f8a46e3613178995dbb4a28dd797810c989f0fa
EverardoMeireles <everardomeireles@gmail.com>
2023-08-21 01:05:41 +0200

    Added the files for the languages

---
+++ src/TranslationTable.jsx
@@ -0,0 +1,28 @@
+export const TranslationTable = {
+        "English" : {
+            "Menu_MainMenu" : "Index",
+            "Menu_Education" : "Education",
+            "Menu_Skills" : "Skills",
+            "Menu_ProfessionalExperience" : "Professional Experience",
+            "Menu_ProspereITB" : "Prospere ITB",
+            "Menu_DRIM" : "DRIM",
+            "Menu_Everial" : "Everial",
+            "Menu_BresilEcoBuggy" : "Brésil Eco Buggy",
+            "Menu_EFN1" : "EFN project PPMS",
+            "Menu_EFN2" : "EFN teacher",
+    
+        },
+    
+        "French" : {
+            "Menu_MainMenu" : "Menu Principal",
+            "Menu_Education" : "Formation",
+            "Menu_Skills" : "Compétences",
+            "Menu_ProfessionalExperience" : "Expérience Professionnelle",
+            "Menu_ProspereITB" : "Prospere ITB",
+            "Menu_DRIM" : "DRIM",
+            "Menu_Everial" : "Everial",
+            "Menu_BresilEcoBuggy" : "Brésil Eco Buggy",
+            "Menu_EFN1" : "EFN projet PPMS",
+            "Menu_EFN2" : "EFN professeur",
+        }
+    }
