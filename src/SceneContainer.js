78993854f2bca05098cec39a6cd44f07cdac77f1
EverardoMeireles <everardomeireles@gmail.com>
2023-10-30 18:56:09 +0100

    Added responsive and translation fields, minor improvements

--- src/SceneContainer.js
+++ src/SceneContainer.js
@@ -41,68 +41,117 @@ export function SceneContainer(props) {
 // });
 <VideoLoader rotation={[0, Math.PI/2, 0]} position={[-13.5, 46.2, -17.1]} planeDimensions={[31, 16.1]}></VideoLoader>
 
-var titlePosition0, titleScale0, titlePosition1, titleScale1,
-fTextText0, fTextPosition0, fTextScale0, fTextLettersPerUnit0, fTextText1, fTextPosition1, fTextScale1, fTextLettersPerUnit1, fTextPosition2, fTextScale2, fTextLettersPerUnit2, fTextPosition3, fTextScale3, fTextLettersPerUnit3,
-FloatingTextPosition,
-videoLoaderPosition
+var fadingTitlePosition0, fadingTitleScale0, fadingTitlePosition1, fadingTitleScale1,
+fadingTextText0, fadingTextPosition0, fadingTextScale0, fadingTextLettersPerUnit0, fadingTextText1, fadingTextPosition1, fadingTextScale1, fadingTextText2, 
+fadingTextPosition2, fadingTextScale2, fadingTextText3, fadingTextPosition3, fadingTextScale3, fadingTextText4, fadingTextPosition4, fadingTextScale4, 
+fadingTextText5, fadingTextPosition5, fadingTextScale5, FloatingTextSkillsPosition
 
 
 //width
 if(window.innerWidth < 500){
-    titlePosition0 = ResponsiveTable["titlePosition0"]/*[170, 148, 58]*/
-    titleScale0 = 17
-    titlePosition1 = [170, 146, 59.5]
-    titleScale1 = 17
-    fTextText0 = "prospere_itb_presentation_mobile"
-    fTextPosition0 = [1, 4, -82]
-    fTextScale0 = 2
-    fTextLettersPerUnit0 = 5
-    fTextText1 = "drim_presentation_mobile"
-    fTextPosition1 = [11.5, 4.5, -95]
+    fadingTitlePosition0 = ResponsiveTable["Mobile"]["fadingTitlePosition0"]
+    fadingTitleScale0 = ResponsiveTable["Mobile"]["fadingTitleScale0"]
+    fadingTitlePosition1 = ResponsiveTable["Mobile"]["fadingTitlePosition1"]
+    fadingTitleScale1 = ResponsiveTable["Mobile"]["fadingTitleScale1"]
+    fadingTextText0 = ResponsiveTable["Mobile"]["fadingTextText0"]
+    fadingTextPosition0 = ResponsiveTable["Mobile"]["fadingTextPosition0"]
+    fadingTextScale0 = ResponsiveTable["Mobile"]["fadingTextScale0"]
+    fadingTextLettersPerUnit0 = ResponsiveTable["Mobile"]["fadingTextLettersPerUnit0"]
+    fadingTextText1 = ResponsiveTable["Mobile"]["fadingTextText1"]
+    fadingTextPosition1 = ResponsiveTable["Mobile"]["fadingTextPosition1"]
+    fadingTextScale1 = ResponsiveTable["Mobile"]["fadingTextScale1"]
+    fadingTextText2 = ResponsiveTable["Mobile"]["fadingTextText2"]
+    fadingTextPosition2 = ResponsiveTable["Mobile"]["fadingTextPosition2"]
+    fadingTextScale2 = ResponsiveTable["Mobile"]["fadingTextScale2"]
+    fadingTextText3 = ResponsiveTable["Mobile"]["fadingTextText3"]
+    fadingTextPosition3 = ResponsiveTable["Mobile"]["fadingTextPosition3"]
+    fadingTextScale3 = ResponsiveTable["Mobile"]["fadingTextScale3"]
+    fadingTextText4 = ResponsiveTable["Mobile"]["fadingTextText3"]
+    fadingTextPosition4 = ResponsiveTable["Mobile"]["fadingTextPosition3"]
+    fadingTextScale4 = ResponsiveTable["Mobile"]["fadingTextScale3"]
+    fadingTextText5 = ResponsiveTable["Mobile"]["fadingTextText3"]
+    fadingTextPosition5 = ResponsiveTable["Mobile"]["fadingTextPosition3"]
+    fadingTextScale5 = ResponsiveTable["Mobile"]["fadingTextScale3"]
+    FloatingTextSkillsPosition = ResponsiveTable["Mobile"]["FloatingTextSkillsPosition"]
 
 }else if(window.width < 1200){
+    fadingTitlePosition0 = ResponsiveTable["Tablet"]["fadingTitlePosition0"]
+    fadingTitleScale0 = ResponsiveTable["Tablet"]["fadingTitleScale0"]
+    fadingTitlePosition1 = ResponsiveTable["Tablet"]["fadingTitlePosition1"]
+    fadingTitleScale1 = ResponsiveTable["Tablet"]["fadingTitleScale1"]
+    fadingTextText0 = ResponsiveTable["Tablet"]["fadingTextText0"]
+    fadingTextPosition0 = ResponsiveTable["Tablet"]["fadingTextPosition0"]
+    fadingTextScale0 = ResponsiveTable["Tablet"]["fadingTextScale0"]
+    fadingTextLettersPerUnit0 = ResponsiveTable["Tablet"]["fadingTextLettersPerUnit0"]
+    fadingTextText1 = ResponsiveTable["Tablet"]["fadingTextText1"]
+    fadingTextPosition1 = ResponsiveTable["Tablet"]["fadingTextPosition1"]
+    fadingTextScale1 = ResponsiveTable["Tablet"]["fadingTextScale1"]
+    fadingTextText2 = ResponsiveTable["Tablet"]["fadingTextText2"]
+    fadingTextPosition2 = ResponsiveTable["Tablet"]["fadingTextPosition2"]
+    fadingTextScale2 = ResponsiveTable["Tablet"]["fadingTextScale2"]
+    fadingTextText3 = ResponsiveTable["Tablet"]["fadingTextText3"]
+    fadingTextPosition3 = ResponsiveTable["Tablet"]["fadingTextPosition3"]
+    fadingTextScale3 = ResponsiveTable["Tablet"]["fadingTextScale3"]
+    fadingTextText4 = ResponsiveTable["Tablet"]["fadingTextText3"]
+    fadingTextPosition4 = ResponsiveTable["Tablet"]["fadingTextPosition3"]
+    fadingTextScale4 = ResponsiveTable["Tablet"]["fadingTextScale3"]
+    fadingTextText5 = ResponsiveTable["Tablet"]["fadingTextText3"]
+    fadingTextPosition5 = ResponsiveTable["Tablet"]["fadingTextPosition3"]
+    fadingTextScale5 = ResponsiveTable["Tablet"]["fadingTextScale3"]
+    FloatingTextSkillsPosition = ResponsiveTable["Tablet"]["FloatingTextSkillsPosition"]
+
 
 }else if(window.width <= 1920){
-    titlePosition0 = [170, 148, 58]
-    titleScale0 = 17
-    titlePosition1 = [170, 146, 59.5]
-    titleScale1 = 17
-    fTextPosition0 = [1, 4, -82]
-    fTextScale0 = 2
-    fTextLettersPerUnit0 = 5
-    // textToFade0 = "prospere_itb_presentation_mobile"
+    fadingTitlePosition0 = ResponsiveTable["Widescreen"]["fadingTitlePosition0"]
+    fadingTitleScale0 = ResponsiveTable["Widescreen"]["fadingTitleScale0"]
+    fadingTitlePosition1 = ResponsiveTable["Widescreen"]["fadingTitlePosition1"]
+    fadingTitleScale1 = ResponsiveTable["Widescreen"]["fadingTitleScale1"]
+    fadingTextText0 = ResponsiveTable["Widescreen"]["fadingTextText0"]
+    fadingTextPosition0 = ResponsiveTable["Widescreen"]["fadingTextPosition0"]
+    fadingTextScale0 = ResponsiveTable["Widescreen"]["fadingTextScale0"]
+    fadingTextText1 = ResponsiveTable["Widescreen"]["fadingTextText1"]
+    fadingTextPosition1 = ResponsiveTable["Widescreen"]["fadingTextPosition1"]
+    fadingTextScale1 = ResponsiveTable["Widescreen"]["fadingTextScale1"]
+    fadingTextText2 = ResponsiveTable["Widescreen"]["fadingTextText2"]
+    fadingTextPosition2 = ResponsiveTable["Widescreen"]["fadingTextPosition2"]
+    fadingTextScale2 = ResponsiveTable["Widescreen"]["fadingTextScale2"]
+    fadingTextText3 = ResponsiveTable["Widescreen"]["fadingTextText3"]
+    fadingTextPosition3 = ResponsiveTable["Widescreen"]["fadingTextPosition3"]
+    fadingTextScale3 = ResponsiveTable["Widescreen"]["fadingTextScale3"]
+    fadingTextText4 = ResponsiveTable["Widescreen"]["fadingTextText3"]
+    fadingTextPosition4 = ResponsiveTable["Widescreen"]["fadingTextPosition3"]
+    fadingTextScale4 = ResponsiveTable["Widescreen"]["fadingTextScale3"]
+    fadingTextText5 = ResponsiveTable["Widescreen"]["fadingTextText3"]
+    fadingTextPosition5 = ResponsiveTable["Widescreen"]["fadingTextPosition3"]
+    fadingTextScale5 = ResponsiveTable["Widescreen"]["fadingTextScale3"]
+    FloatingTextSkillsPosition = ResponsiveTable["Widescreen"]["FloatingTextSkillsPosition"]
+
 
 }
-// titlePosition0 = [170, 148, 58]
-// titleScale0 = 17
-// titlePosition1 = [170, 146, 59.5]
-// titleScale1 = 17
-
     return(
         <>
-            <FadingTitle responsive={props.responsive} initialPosition={titlePosition0} {...{useStore}} scale = {titleScale0} text={"Everardo Meireles"} visible={false} textColor={"#000000"} delay={4800} transitionDuration = {1500} />
-            <FadingTitle initialPosition={titlePosition1} {...{useStore}} scale = {titleScale1} text={"Developpeur Fullstack"} visible={false} textColor={"#000000"} delay={5400} transitionDuration = {1500} />
+            <FadingTitle {...{useStore}} initialPosition={fadingTitlePosition0} scale = {fadingTitleScale0} text={"Everardo Meireles"} visible={false} textColor={"#000000"} delay={4800} transitionDuration = {1500} />
+            <FadingTitle {...{useStore}} initialPosition={fadingTitlePosition1} scale = {fadingTitleScale1} text={"Developpeur Fullstack"} visible={false} textColor={"#000000"} delay={5400} transitionDuration = {1500} />
             <PathNavigation {...{useStore}} possiblePaths = {["MainMenu", "Education", "Skills", "ProfessionalExpProjects0"]} />
             <Suspense fallback = {null} >
                 {(finishedBenchmark == false && config.check_graphics) && <GraphicalModeSetter {...{useStore}} numberOfPasses={1} fpsToDecreaseGraphics={55} />}
                 <Environment files={process.env.PUBLIC_URL + "/textures/dikhololo_night_1k.hdr"} background />
                 <Environment files={process.env.PUBLIC_URL + "/textures/kloofendal_48d_partly_cloudy_puresky_1k.hdr"} background={"only"} />
                 <Camera {...{useStore}}></Camera>
-                <FadingText {...{useStore}} textToFade={TranslationTable[currentLanguage][fTextText0]} lettersPerUnit = {fTextLettersPerUnit0} textModelMenu="ProfessionalExpProjects0" scale={fTextScale0} initialPosition={fTextPosition0} rotation={2 * Math.PI} visible={false} textColor={"#FFFFFF"} manualLineBreaks={true} />
-                <FadingText {...{useStore}} textToFade={TranslationTable[currentLanguage][fTextText1]} textModelMenu="ProfessionalExpProjects1" scale={1.8} initialPosition={fTextPosition1} rotation={Math.PI/2} visible={false} textColor={"#FFFFFF"} manualLineBreaks={true} />
-                <FadingText {...{useStore}} textToFade={Text["EVERIAL-PRESENTATION"]["FR"]} textModelMenu="ProfessionalExpProjects2" scale={2} initialPosition={[-2, 5.5, -105]} rotation={Math.PI} visible={false} textColor={"#FFFFFF"} manualLineBreaks={true} />
-                <FadingText {...{useStore}} textToFade={Text["BRESIL-ECOBUGGY-PRESENTATION"]["FR"]} textModelMenu="ProfessionalExpProjects3" lettersPerUnit={10} scale={1.7} initialPosition={[-11, 7, -94.5]} rotation={3*(Math.PI/2)} visible={false} textColor={"#FFFFFF"} manualLineBreaks={true} />
-                <FadingText {...{useStore}} textToFade={Text["EFN1-PRESENTATION"]["FR"]} textModelMenu="ProfessionalExpProjects4" lettersPerUnit={9} scale={1.8} initialPosition={[-0.5, 25, -82.2]} rotation={2 * Math.PI} visible={false} textColor={"#FFFFFF"} manualLineBreaks={true} />
-                <FadingText {...{useStore}} textToFade={Text["EFN2-PRESENTATION"]["FR"]} textModelMenu="ProfessionalExpProjects5" scale={2} lettersPerUnit={7} initialPosition={[11.5, 25, -95]} rotation={Math.PI/2} visible={false} textColor={"#FFFFFF"} manualLineBreaks={true} />
+                <FadingText {...{useStore}} textToFade={TranslationTable[currentLanguage][fadingTextText0]} lettersPerUnit = {5} textModelMenu="ProfessionalExpProjects0" scale={fadingTextScale0} initialPosition={fadingTextPosition0} rotation={2 * Math.PI} visible={false} textColor={"#FFFFFF"} manualLineBreaks={true} />
+                <FadingText {...{useStore}} textToFade={TranslationTable[currentLanguage][fadingTextText1]} textModelMenu="ProfessionalExpProjects1" scale={fadingTextScale1} initialPosition={fadingTextPosition1} rotation={Math.PI/2} visible={false} textColor={"#FFFFFF"} manualLineBreaks={true} />
+                <FadingText {...{useStore}} textToFade={TranslationTable[currentLanguage][fadingTextText2]} textModelMenu="ProfessionalExpProjects2" scale={fadingTextScale2} initialPosition={fadingTextPosition2} rotation={Math.PI} visible={false} textColor={"#FFFFFF"} manualLineBreaks={true} />
+                <FadingText {...{useStore}} textToFade={TranslationTable[currentLanguage][fadingTextText3]} textModelMenu="ProfessionalExpProjects3" lettersPerUnit={10} scale={fadingTextScale3} initialPosition={fadingTextPosition3} rotation={3*(Math.PI/2)} visible={false} textColor={"#FFFFFF"} manualLineBreaks={true} />
+                <FadingText {...{useStore}} textToFade={TranslationTable[currentLanguage][fadingTextText4]} textModelMenu="ProfessionalExpProjects4" lettersPerUnit={9} scale={fadingTextScale4} initialPosition={fadingTextPosition4} rotation={2 * Math.PI} visible={false} textColor={"#FFFFFF"} manualLineBreaks={true} />
+                <FadingText {...{useStore}} textToFade={TranslationTable[currentLanguage][fadingTextText5]} textModelMenu="ProfessionalExpProjects5" scale={fadingTextScale5} lettersPerUnit={7} initialPosition={fadingTextPosition5} rotation={Math.PI/2} visible={false} textColor={"#FFFFFF"} manualLineBreaks={true} />
                 {/* <FadingText {...{useStore}} textModelMenu="ProfessionalExpProjects6" initialPosition={[-4, 28, -105]} rotation={Math.PI} visible={false} textColor={"#FFFFFF"} manualLineBreaks={true} />
                 <FadingText {...{useStore}} textModelMenu="ProfessionalExpProjects7" initialPosition={[-11, 28, -90]} rotation={3*(Math.PI/2)} visible={false} textColor={"#FFFFFF"} manualLineBreaks={true} />
                 <FadingText {...{useStore}} textModelMenu="ProfessionalExpProjects8" initialPosition={[4, 49, -82.2]} rotation={2 * Math.PI} visible={false} textColor={"#FFFFFF"} manualLineBreaks={true} />
                 <FadingText {...{useStore}} textModelMenu="ProfessionalExpProjects9" initialPosition={[11, 49, -97]} rotation={Math.PI/2} visible={false} textColor={"#FFFFFF"} manualLineBreaks={true} />
                 <FadingText {...{useStore}} textModelMenu="ProfessionalExpProjects10" initialPosition={[-4, 49, -105]} rotation={Math.PI} visible={false} textColor={"#FFFFFF"} manualLineBreaks={true} />
                 <FadingText {...{useStore}} textModelMenu="ProfessionalExpProjects11" initialPosition={[-11, 49, -90]} rotation={3*(Math.PI/2)} visible={false} textColor={"#FFFFFF"} manualLineBreaks={true} /> */}
-                <FloatingTextSkills {...{useStore}} initialPosition={[-9, 30, -15]} />
-                {/* <Title initialPosition={[0, 4, 5.4]}/> */}
-                <OrbitingMenu visible={finishedBenchmark == true ? true : false} orbitDistance={7.5} orbitCenterPosition={[-17, 97, 27]}/>
+                <FloatingTextSkills {...{useStore}} initialPosition={[-9, 30, -15]} textPosition = {FloatingTextSkillsPosition} />
+                <OrbitingMenu visible={ (config.check_graphics == false || finishedBenchmark == true) ? true : false} orbitDistance={7.5} orbitCenterPosition={[-17, 97, 27]}/>
                 <ambientLight intensity={1}></ambientLight>
                 <Suspense>
                     <SimpleLoader modelName={"NewthreeJsScene.glb"} ambientOcclusionIntensivity={2.5}></SimpleLoader>
--- src/components/FloatingTextSkills.jsx
+++ src/components/FloatingTextSkills.jsx
@@ -9,12 +9,11 @@ export function FloatingTextSkills(props) {
     const {rotation = Math.PI/2} = props;
     const {textCurveSergments = 12} = props;
     const {font = process.env.PUBLIC_URL + "KFOmCnqEu92Fr1Mu4mxM.woff"} = props;
+    const {textPosition = [[0, 5, 0], [-3, 0, 3], [2, 5, 8], [0, 0, 4], [3, 3, 0], [-6, 3, 5], [5,0, 5], [4, 2, 6], [3, 3, 8], [0, 0, 8]]} = props;
 
-    const skills = props.useStore((state) => state.skills);
     const setSkillHovered = props.useStore((state) => state.setSkillHovered); 
 
     const sizeLarge = 0.600
-    const textPosition = [[0, 5, 0], [-3, 0, 3], [2, 5, 8], [0, 0, 4], [3, 3, 0], [-6, 3, 5], [5,0, 5], [4, 2, 6], [3, 3, 8], [0, 0, 8]]
     const textContents = ["Python", "C#", "JavaScript", "React", "Three.js", "blender", "SQL", "HTML/CSS", "anglais", "portugais"]// allemand(compris uniquement), API REST, async, arduino, flask 
     const textColorCat = ["red", "red", "red", "blue", "blue", "green", "yellow", "yellow", "black", "black"]
 
@@ -92,15 +91,10 @@ export function FloatingTextSkills(props) {
 
     return(
     <mesh position = {initialPosition} ref = {callbackRef}>
-        {/* {components} */}
         <Float>
             <a.mesh position = {textPosition[0]} scale = {springScale.scale0} onPointerOver={() => {setHover0(true); setSkillHovered(textContents[0]);}} onPointerOut={() => setHover0(false)}>
                 <planeGeometry args={hitBoxSize} />
                 <meshBasicMaterial visible={false} />
-                {/* <Text3D position={[-1,-0.2,0]} font = {process.env.PUBLIC_URL + fontFileName} size = {sizeLarge} height = {0.065} curveSegments = {textCurveSergments}>
-                    {textContents[0]}
-                    <a.meshStandardMaterial color={springColor.color0}/>
-                </Text3D> */}
                 <Text
                     font={font}
                     scale={[8, 8, 8]}
@@ -116,10 +110,6 @@ export function FloatingTextSkills(props) {
             <a.mesh position = {textPosition[1]} scale = {springScale.scale1} onPointerOver={() => {setHover1(true); setSkillHovered(textContents[1]);}} onPointerOut={() => setHover1(false)}>
                 <planeGeometry args={hitBoxSize} />
                 <meshBasicMaterial visible={false} />
-                {/* <Text3D position={[-1,-0.2,0]} font = {process.env.PUBLIC_URL + fontFileName} size = {sizeLarge} height = {0.065} curveSegments = {textCurveSergments}>
-                {textContents[1]}
-                    <a.meshStandardMaterial color={springColor.color1}/>
-                </Text3D> */}
                 <Text
                     font={font}
                     scale={[8, 8, 8]}
@@ -135,10 +125,6 @@ export function FloatingTextSkills(props) {
             <a.mesh position = {textPosition[2]} scale = {springScale.scale2} onPointerOver={() => {setHover2(true); setSkillHovered(textContents[2]);}} onPointerOut={() => setHover2(false)}>
                 <planeGeometry args={hitBoxSize} />
                 <meshBasicMaterial visible={false} />
-                {/* <Text3D position={[-1,-0.2,0]} font = {process.env.PUBLIC_URL + fontFileName} size = {sizeLarge} height = {0.065} curveSegments = {textCurveSergments}>
-                    {textContents[2]}
-                    <a.meshStandardMaterial color={springColor.color2}/>
-                </Text3D> */}
                 <Text
                     font={font}
                     scale={[8, 8, 8]}
@@ -154,10 +140,6 @@ export function FloatingTextSkills(props) {
             <a.mesh position = {textPosition[3]} scale = {springScale.scale3} onPointerOver={() => {setHover3(true); setSkillHovered(textContents[3]);}} onPointerOut={() => setHover3(false)}>
                 <planeGeometry args={hitBoxSize} />
                 <meshBasicMaterial visible={false} />
-                {/* <Text3D position={[-1,-0.2,0]} font = {process.env.PUBLIC_URL + fontFileName} size = {sizeLarge} height = {0.065} curveSegments = {textCurveSergments}>
-                    {textContents[3]}
-                    <a.meshStandardMaterial color={springColor.color3}/>
-                </Text3D> */}
                 <Text
                     font={font}
                     scale={[8, 8, 8]}
@@ -173,10 +155,6 @@ export function FloatingTextSkills(props) {
             <a.mesh position = {textPosition[4]} scale = {springScale.scale4} onPointerOver={() => {setHover4(true); setSkillHovered(textContents[4]);}} onPointerOut={() => setHover4(false)}>
                 <planeGeometry args={hitBoxSize} />
                 <meshBasicMaterial visible={false} />
-                {/* <Text3D position={[-1,-0.2,0]} font = {process.env.PUBLIC_URL + fontFileName} size = {sizeLarge} height = {0.065} curveSegments = {textCurveSergments}>
-                    {textContents[4]}
-                    <a.meshStandardMaterial color={springColor.color4}/>
-                </Text3D> */}
                 <Text
                     font={font}
                     scale={[8, 8, 8]}
@@ -192,10 +170,6 @@ export function FloatingTextSkills(props) {
             <a.mesh position = {textPosition[5]} scale = {springScale.scale5} onPointerOver={() => {setHover5(true); setSkillHovered(textContents[5]);}} onPointerOut={() => setHover5(false)}>
                 <planeGeometry args={hitBoxSize} />
                 <meshBasicMaterial visible={false} />
-                {/* <Text3D position={[-1,-0.2,0]} font = {process.env.PUBLIC_URL + fontFileName} size = {sizeLarge} height = {0.065} curveSegments = {textCurveSergments}>
-                    {textContents[5]}
-                    <a.meshStandardMaterial color={springColor.color5}/>
-                </Text3D> */}
                 <Text
                     font={font}
                     scale={[8, 8, 8]}
@@ -211,10 +185,6 @@ export function FloatingTextSkills(props) {
             <a.mesh position = {textPosition[6]} scale = {springScale.scale6} onPointerOver={() => {setHover6(true); setSkillHovered(textContents[6]);}} onPointerOut={() => setHover6(false)}>
                 <planeGeometry args={hitBoxSize} />
                 <meshBasicMaterial visible={false} />
-                {/* <Text3D position={[-1,-0.2,0]} font = {process.env.PUBLIC_URL + fontFileName} size = {sizeLarge} height = {0.065} curveSegments = {textCurveSergments}>
-                    {textContents[6]}
-                    <a.meshStandardMaterial color={springColor.color6}/>
-                </Text3D> */}
                 <Text
                     font={font}
                     scale={[8, 8, 8]}
@@ -230,10 +200,6 @@ export function FloatingTextSkills(props) {
             <a.mesh position = {textPosition[7]} scale = {springScale.scale7} onPointerOver={() => {setHover7(true); setSkillHovered(textContents[7]);}} onPointerOut={() => setHover7(false)}>
                 <planeGeometry args={hitBoxSize} />
                 <meshBasicMaterial visible={false} />
-                {/* <Text3D position={[-1,-0.2,0]} font = {process.env.PUBLIC_URL + fontFileName} size = {sizeLarge} height = {0.065} curveSegments = {textCurveSergments}>
-                    {textContents[7]}
-                    <a.meshStandardMaterial color={springColor.color7}/>
-                </Text3D> */}
                 <Text
                     font={font}
                     scale={[8, 8, 8]}
@@ -249,10 +215,6 @@ export function FloatingTextSkills(props) {
             <a.mesh position = {textPosition[8]} scale = {springScale.scale8} onPointerOver={() => {setHover8(true); setSkillHovered(textContents[8]);}} onPointerOut={() => setHover8(false)}>
                 <planeGeometry args={hitBoxSize} />
                 <meshBasicMaterial visible={false} />
-                {/* <Text3D position={[-1,-0.2,0]} font = {process.env.PUBLIC_URL + fontFileName} size = {sizeLarge} height = {0.065} curveSegments = {textCurveSergments}>
-                    {textContents[8]}
-                    <a.meshStandardMaterial color={springColor.color8}/>
-                </Text3D> */}
                 <Text
                     font={font}
                     scale={[8, 8, 8]}
@@ -268,10 +230,6 @@ export function FloatingTextSkills(props) {
             <a.mesh position = {textPosition[9]} scale = {springScale.scale9} onPointerOver={() => {setHover9(true); setSkillHovered(textContents[9]);}} onPointerOut={() => setHover9(false)}>
                 <planeGeometry args={hitBoxSize} />
                 <meshBasicMaterial visible={false} />
-                {/* <Text3D position={[-1,-0.2,0]} font = {process.env.PUBLIC_URL + fontFileName} size = {sizeLarge} height = {0.065} curveSegments = {textCurveSergments}>
-                    {textContents[9]}
-                    <a.meshStandardMaterial color={springColor.color9}/>
-                </Text3D> */}
                 <Text
                     font={font}
                     scale={[8, 8, 8]}
@@ -283,16 +241,6 @@ export function FloatingTextSkills(props) {
                 </Text>
             </a.mesh>
         </Float>
-        {/* <Float>
-            <a.mesh position = {textPosition[10]} scale = {springScale.scale10} onPointerOver={() => setHover10(true)} onPointerOut={() => setHover10(false)}>
-                <planeGeometry args={hitBoxSize} />
-                <meshBasicMaterial visible={false} />
-                <Text3D position={[-1,-0.2,0]} font = {process.env.PUBLIC_URL + fontFileName} size = {sizeLarge} height = {0.065} curveSegments = {textCurveSergments}>
-                    {textContents[10]}
-                    <a.meshStandardMaterial color={springColor.color10}/>
-                </Text3D>
-            </a.mesh>
-        </Float> */}
     </mesh>
     );
 }
