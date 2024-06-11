6d1055e4a69dbec15431ad2cac6363d11bd38339
EverardoMeireles <everardomeireles@gmail.com>
2024-06-11 19:43:07 +0200

    Organized SimpleLoader.jsx with better comments

--- src/components/SimpleLoader.jsx
+++ src/components/SimpleLoader.jsx
@@ -9,182 +9,81 @@ export const SimpleLoader = React.memo((props) => {
     const useStore = props.useStore;
     const {position = [0, 0, 0]} = props;
     const {sceneName = "threeJsScene.glb"} = props; // the model/scene's name
+    
+    // feature: play animation(s) on trigger
     const {animationToPlay = "idle"} = props; // the name of the animation to be played
     const {loopMode = "noLoop"} = props; // Looping mode: "Loop", "noLoop" and "noLoopAndReset(return to first frame when animation finished, doesn't work with playDirection = -1)"
     const {playDirection = 1} = props; // 1 for normal playback, -1 to play the animation backwards
     const {autoPlay = true} = props; // Automatically plays the animation when component loads
     const {animationTrigger = false} = props; // the trigger state to make the animation play
-    const {useAo = true} = props; // use ambient occlusion if the loaded scene supports it
-    const {ambientOcclusionIntensity = 1} = props; // intensity of the ambient occlusion
+    
+    // feature : hover
     const {hover = true} = props; // wether the scene or objects within the scene should be highlighted on mouse hover
     const {hoverAffectedObjects = []} = props; // Objects that are affected by the hover effect and the color that they will "glow"
     const {hoverLinkedObjects = [[]]} = props; // Objects that are affected by the hover effect and the color that they will "glow"
     const {hoveredObject = undefined} = props; // prop passed by the raycaster, its value its the current hovered object
+    
+    // Feature : toggles trigger in the middle of animation
     const {animationTimesToTrigger = {}} = props; // Times where the triggers are set to activate, to be used with animationTriggerNames. EX:{"CharacterAction": 0.50}
     const {animationTriggerNames = {}} = props; // Triggers to be activated by the times, to be used with animationTimesToTrigger. EX:{"CharacterAction": "trigger1"}
+    
+    // Feature : hide or reveal mesh on trigger(Assumes scale transform has been applied)
+    const {objectsHideRevealTriggers = {"Cube0001":"trigger1"}} = props; // Triggers to reveal hidden objects. Ex: {"Wardrobe001":"trigger3"}
+    const {objectHideRevealScaleUpSpeed = 0.05} = props; // speed of the scale transition animation
 
-    const {objectsRevealTriggers = {"BookShelf001":"trigger1", "BookShelf002":"trigger2"}} = props; // Triggers to reveal hidden objects
-    const {objectRevealScaleUpSpeed = 0.05} = props; // Triggers to be activated by the times, to be used with animationTimesToTrigger. EX:{"CharacterAction": "trigger1"}
+    // feature : Ambient occlusion
+    const {useAo = true} = props; // use ambient occlusion if the loaded scene supports it
+    const {ambientOcclusionIntensity = 1} = props; // intensity of the ambient occlusion
 
 
-    var objectRevealDirections = {}; // store the factor of the scale up or down effects, 1 or -1. EX:{"BookShelf" : -1, "Table" : 1}
 
-    let fpsTriggerAnimationIsPlaying = false;
-    let totalTriggerAnimationFrames;
-    let currentAnimationFrame = 0;
+    //////////////////////////////////////////////////////////
+    ///////////// Variables, states and refs /////////////////
+    //////////////////////////////////////////////////////////
 
+    // variables for the animation trigger feature
     let normalizedClipTime; 
     let animationCurrentTime;
     let animationDuration;
-    
-    const [triggerScaleAnimation, setTriggerScaleAnimation] = useState(false);
-    const [animationFadeOut, setAnimationFadeOut] = useState(false);
 
-    const [childObject, setChildObject] = useState(false);
-    const [currentLinkedObjects, setCurrentLinkedObjects] = useState([]);
+    // variables for the toggle trigger in the middle of animation feature( Useless??? )
+    let fpsTriggerAnimationIsPlaying = false;
+    let currentAnimationFrame = 0;
+
+    // Variable hide or reveal mesh on trigger feature
+    let objectHideRevealDirections = {}; // store the factor of the scale up or down effects, 1 or -1. EX:{"BookShelf" : -1, "Table" : 1}
+
+    // Load Model
+    const gltf = useLoader(GLTFLoader, process.env.PUBLIC_URL + '/models/' + sceneName);
 
     const toggleTrigger = useStore((state) => state.toggleTrigger);
     const triggers = useStore((state) => state.triggers);
 
     const setInitialSceneLoaded = useStore((state) => state.setInitialSceneLoaded);
 
-    // Load Model
-    const gltf = useLoader(GLTFLoader, process.env.PUBLIC_URL + '/models/' + sceneName);
-
     // initialize the animation mixer
     const mixer = useRef(new THREE.AnimationMixer(gltf.scene));
 
+    // ref and state of the HideReveal feature
+    const fadeInObjectsKeysRef = useRef([]);
+    const [fade, setFadeIn] = useState(false);
+
+    // states for the hover feature
+    const [triggerScaleAnimation, setTriggerScaleAnimation] = useState(false);
+    const [animationFadeOut, setAnimationFadeOut] = useState(false);
+
+    const [childObject, setChildObject] = useState(false);
+    const [currentLinkedObjects, setCurrentLinkedObjects] = useState([]);
+
 
     
-
-    const fadeInObjectsKeysRef = useRef([]);
-    const [fadeIn, setFadeIn] = useState(false);
-
-    // Hide market objects at the start
-    useEffect(() => {
-        console.log(gltf)
-        Object.entries(objectsRevealTriggers).forEach(([key, value]) => {
-            gltf.scene.traverse((child) => {
-                if (child.isMesh && child.name === key) {
-                    console.log(child.name)
-                  child.scale.set(0, 0, 0);
-                }
-              });
-        });
-      }, [gltf]);
-
-      // set scale factors of marked objects
-      useEffect(() => {
-        console.log(gltf)
-        Object.entries(objectsRevealTriggers).forEach(([key, value]) => {
-            gltf.scene.traverse((child) => {
-                if (child.isMesh && child.name === key) {
-                    if(child.scale["x"] <= 0){
-                        objectRevealDirections[key] = 1;
-                    }
-                    else{
-                        objectRevealDirections[key] = -1;
-                    }
-                    
-                }
-              });
-        });
-      }, [gltf, objectRevealDirections]);
-
-    // trigger the fade in scale animation(trigger is set to true)
-    useEffect(() => {
-        fadeInObjectsKeysRef.current = Object.entries(objectsRevealTriggers)
-        .filter(([key, value]) => triggers[value] === true)
-        .map(([key]) => key);
-
-        // check whether to proceed based on reversability
-        // fadeInObjectsKeysRef.current.forEach((key)=>{
-
-        // })
-
-        if(fadeInObjectsKeysRef.current.length > 0){
-            console.log(fadeInObjectsKeysRef.current)
-            setFadeIn(true);
-            console.log(fadeIn)
-        }
-        //  else{
-        //     setFadeIn(false);
-        // }
-    }, [objectsRevealTriggers, fadeInObjectsKeysRef.current, fadeIn, objectRevealDirections]);
-
-    var skip = false;
-    // Fade-in Animation
-    useFrame((state, delta) => {
-
-        if (fadeIn) {
-            // console.log(fadeIn)
-                fadeInObjectsKeysRef.current.forEach((key)=>{
-                    gltf.scene.traverse((child) => {
-                        // console.log(child)
-                        // Don't fade in or out if already done and not set to be reversible
-                        if (child.isMesh && child.name === key) {
-                                if((child.scale["x"] >= 1 && objectRevealDirections[key] == 1) || (child.scale["x"] <= 0 && objectRevealDirections[key] == -1)){
-                                    return
-                                }
-                            
-                            
-                            // console.log(child)
-                            child.scale.set(child.scale["x"] + objectRevealScaleUpSpeed * objectRevealDirections[key], child.scale["y"] + objectRevealScaleUpSpeed * objectRevealDirections[key], child.scale["z"] + objectRevealScaleUpSpeed * objectRevealDirections[key]);
-                            console.log(child.scale)
-                            if(child.scale["x"] >= 1 || child.scale["x"] <= 0){
-                                toggleTrigger(objectsRevealTriggers[key])
-                                fadeInObjectsKeysRef.current.splice(fadeInObjectsKeysRef.current.indexOf(key), 1);
-                                objectsRevealTriggers[key] = false
-                                console.log(objectsRevealTriggers);
-                                console.log(fadeInObjectsKeysRef.current);
-
-                            }
-
-                            if(fadeInObjectsKeysRef.current.length == 0){
-                                setFadeIn(false);
-                                return
-                            }
-                        }
-                      });
-
-                }
-                )}
-      });
-
-    // plays the animation
-    function playAnimation(){
-        gltf.animations.forEach(clip => {
-            if(animationToPlay.includes(clip.name)){
-                const action = mixer.current.clipAction(clip);
-                setupLoopMode(action)
-                setupReverseMode(action)
-                action.play();
-                // console.log(animationToPlay)
-            }
-        });
-    }
-
-    useFrame(() => {
-        if(fpsTriggerAnimationIsPlaying){
-            currentAnimationFrame += 1;
-            console.log(currentAnimationFrame)
-        }
-    });
+    //////////////////////////////////////////////////////////
+    // Feature : play animation(s) on trigger
+    //////////////////////////////////////////////////////////
 
     // stops execution if the animation's name string is not inside an array
     if(!Array.isArray(animationToPlay)){
         throw new Error("the animation's name must be inside an array");
-    }
-
-    // 
-
-    // initiate ambient occlusion if useAo is set to true
-    if(useAo) {
-        for( let node in gltf.nodes) {
-            if('material' in gltf.nodes[node]) {
-                gltf.nodes[node].material.aoMapIntensity = ambientOcclusionIntensity;
-            }
-        }
     }
 
     // Checks loopMode before the animation plays
@@ -203,16 +102,6 @@ export const SimpleLoader = React.memo((props) => {
         }
     }
 
-    // Modify this to only reset appropriate objects?
-    // Resets the scale of all objects back to 0
-    function resetObjectsScale(){
-        gltf.scene.traverse((child) => {
-                child.scale.x = 1
-                child.scale.y = 1
-                child.scale.z = 1
-        })
-    }
-
     // start to play the animation when scene loads if the prop autoPlay is true
     useEffect(() => {
         if (autoPlay && gltf.animations.length) {
@@ -226,6 +115,32 @@ export const SimpleLoader = React.memo((props) => {
             playAnimation()
         }
     }, [animationTrigger, animationToPlay]);
+
+    // plays the animation
+    function playAnimation(){
+        gltf.animations.forEach(clip => {
+            if(animationToPlay.includes(clip.name)){
+                const action = mixer.current.clipAction(clip);
+                setupLoopMode(action)
+                setupReverseMode(action)
+                action.play();
+            }
+        });
+    }
+
+    //////////////////////////////////////////////////////////
+    // Feature : Hover
+    //////////////////////////////////////////////////////////
+
+    // Modify this to only reset appropriate objects???
+    // Resets the scale of all objects back to 0
+    function resetObjectsScale(){
+        gltf.scene.traverse((child) => {
+                child.scale.x = 1
+                child.scale.y = 1
+                child.scale.z = 1
+        })
+    }
 
     function getLinkedObjects(object){
         var returnedObjects = [];
@@ -292,6 +207,16 @@ export const SimpleLoader = React.memo((props) => {
         }
     });
 
+    //////////////////////////////////////////////////////////
+    // Feature : toggles trigger in the middle of animation
+    //////////////////////////////////////////////////////////
+
+    // updates deltas for animations
+    useFrame((state, delta) => {
+        mixer.current?.update(delta);
+        AnimationTimeTriggerCheck(mixer);
+    })
+
     // Checks whether to toggle trigger on a set
     function AnimationTimeTriggerCheck(mixer){
         if(Object.keys(animationTriggerNames).length != 0){
@@ -308,21 +233,110 @@ export const SimpleLoader = React.memo((props) => {
                         toggleTrigger(animationTriggerNames[action._clip.name])
                     }
                 }
-                // console.log(triggers[animationTriggerNames[action._clip.name]])
             });
         }
     }
+    
+    //////////////////////////////////////////////////////////
+    // Feature : hide or reveal mesh on trigger
+    //////////////////////////////////////////////////////////
 
-    // updates deltas for animations
-    useFrame((state, delta) => {
-        mixer.current?.update(delta);
-        AnimationTimeTriggerCheck(mixer);
-    })
+    // Hide marked objects at the start
+    useEffect(() => {
+        console.log(gltf)
+        Object.entries(objectsHideRevealTriggers).forEach(([key, value]) => {
+            gltf.scene.traverse((child) => {
+                if (child.isMesh && child.name === key) {
+                    // console.log(child.name)
+                child.scale.set(0, 0, 0);
+                }
+            });
+        });
+    }, [gltf]);
+
+      // set scale factors of marked objects
+    useEffect(() => {
+        // console.log(gltf)
+        Object.entries(objectsHideRevealTriggers).forEach(([key, value]) => {
+            gltf.scene.traverse((child) => {
+                if (child.isMesh && child.name === key) {
+                    if(child.scale["x"] <= 0){
+                        objectHideRevealDirections[key] = 1;
+                    }
+                    else{
+                        objectHideRevealDirections[key] = -1;
+                    }
+                    
+                }
+            });
+        });
+    }, [gltf, objectHideRevealDirections]);
+
+    // trigger the fade in scale animation(trigger is set to true)
+    useEffect(() => {
+        fadeInObjectsKeysRef.current = Object.entries(objectsHideRevealTriggers)
+        .filter(([key, value]) => triggers[value] === true)
+        .map(([key]) => key);
+
+        if(fadeInObjectsKeysRef.current.length > 0){
+            setFadeIn(true);
+        }
+
+    }, [objectsHideRevealTriggers, fadeInObjectsKeysRef.current, fade, objectHideRevealDirections]);
+
+    // Reveal and hide animation
+    useFrame(() => {
+        if (fade) {
+            fadeInObjectsKeysRef.current.forEach((key)=>{
+                gltf.scene.traverse((child) => {
+                    // Don't fade in or out if already done and not set to be reversible
+                    if (child.isMesh && child.name === key) {
+                        if((child.scale["x"] >= 1 && objectHideRevealDirections[key] == 1) || (child.scale["x"] <= 0 && objectHideRevealDirections[key] == -1)){
+                            return
+                        }
+                        
+                        child.scale.set(child.scale["x"] + objectHideRevealScaleUpSpeed * objectHideRevealDirections[key], child.scale["y"] + objectHideRevealScaleUpSpeed * objectHideRevealDirections[key], child.scale["z"] + objectHideRevealScaleUpSpeed * objectHideRevealDirections[key]);
+                        if(child.scale["x"] >= 1 || child.scale["x"] <= 0){
+                            toggleTrigger(objectsHideRevealTriggers[key])
+                            fadeInObjectsKeysRef.current.splice(fadeInObjectsKeysRef.current.indexOf(key), 1);
+                            objectsHideRevealTriggers[key] = false
+                        }
+
+                        if(fadeInObjectsKeysRef.current.length == 0){
+                            setFadeIn(false);
+                            return
+                        }
+                    }
+                });
+            })
+        }
+    });
+
+    // Useless????
+    useFrame(() => {
+        if(fpsTriggerAnimationIsPlaying){
+            currentAnimationFrame += 1;
+        }
+    });
+
+
+    //////////////////////////////////////////////////////////
+    // Feature : Ambient occlusion
+    //////////////////////////////////////////////////////////
+
+    // initiate ambient occlusion if useAo is set to true
+    if(useAo) {
+        for( let node in gltf.nodes) {
+            if('material' in gltf.nodes[node]) {
+                gltf.nodes[node].material.aoMapIntensity = ambientOcclusionIntensity;
+            }
+        }
+    }
 
     useEffect(() => {
         console.log("set initial scene loaded")
         setInitialSceneLoaded(true);
-      }, [gltf, setInitialSceneLoaded]);
+    }, [gltf, setInitialSceneLoaded]);
 
     return (
     <Suspense fallback={null}>
