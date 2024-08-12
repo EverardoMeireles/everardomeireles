53db8b5d3ef1ca72cb70fff44b0703362588321c
EverardoMeireles <everardomeireles@gmail.com>
2024-08-12 14:07:20 +0200

    Added removeFileExtensionString and parseJson and methods to the Helper file

--- src/Helper.js
+++ src/Helper.js
@@ -1,4 +1,4 @@
-    // replace it by store's value later
+// replace it by store's value later
 export const graphicsModes = {
     0:"potato",
     1:"potatoPremium",
@@ -41,4 +41,44 @@ export function increaseOrDecreaseGraphics(currentGraphicalMode, setGraphicalMod
 
 export function getRandomInt(min, max) {
     return Math.random() * (max - min + 1) + min;
+}
+
+export function removeFileExtensionString(inputString) {
+    // Find the last dot in the string
+    const lastDotIndex = inputString.lastIndexOf('.');
+
+    // If a dot is found and it's not the first character
+    if (lastDotIndex > 0) {
+        // Return the string without the extension
+        return inputString.slice(0, lastDotIndex);
+    } else {
+        // If no dot is found, return the original string
+        return inputString;
+    }
+}
+
+// Function to parse the JSON and get a specific object, if specified
+export async function parseJson(filePath, objectToGet = null) {
+    try {
+        const response = await fetch(filePath);
+        if (!response.ok) {
+            console.log('File does not exist.');
+            return null;
+        }
+        const jsonData = await response.json();
+        if (objectToGet) {
+            if (jsonData[objectToGet]) {
+                console.log("json found!")
+                return jsonData[objectToGet];
+            } else {
+                console.log(`${objectToGet} not found in the JSON.`);
+                return null;
+            }
+        } else {
+            return jsonData;
+        }
+    } catch (error) {
+        console.log('Error fetching JSON:', error);
+        return null;
+    }
 }
